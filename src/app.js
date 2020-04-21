584d296b890ff5620eb76650571e03d0164bf3ec
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-04-21 14:02:32 -0400

    added comments logic

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const articlesRouter = require('./articlesRouter')
 const usersRouter = require('./users/users-router')
+const commentsRouter = require('./comments/comments-router')
 
 const app = express()
 
@@ -18,8 +19,8 @@ app.use(helmet())
 app.use(cors())
 
 app.use('/api/articles', articlesRouter)
-
 app.use('/api/users', usersRouter)
+app.use('/api/comments', commentsRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello world')
